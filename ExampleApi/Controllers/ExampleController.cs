4bc2d9497d01a826fe35647edd48581f25e5f9fd
RubenRojasRoman <rrojas@quarksoft.net>
2024-03-07 16:16:33 -0600

    Add project files.

---
+++ ExampleApi/Controllers/ExampleController.cs
@@ -0,0 +1,30 @@
+﻿using ExampleApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<ExampleModel>> GetAll()
+        {
+            List<ExampleModel> values = new();
+            for (int i = 1; i <= 10; i++ )
+            {
+                var example = new ExampleModel
+                {
+                    Id = i,
+                    Name = $"Test {i}",
+                    Description = $"Description {i} test",
+                    Author = $"Author {i} Test",
+                    AuthorId = i
+                };
+                values.Add(example);
+            }
+            return Ok(values);
+        }
+    }
+}
